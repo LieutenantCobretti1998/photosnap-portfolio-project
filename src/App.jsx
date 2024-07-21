178d7798f56275389b3d9cc7fd7ffd302305c5aa
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-07-21 11:56:04 +0400

    First commit in this project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./header/header.jsx";
+import Home from "./homePage-main/main_page_body.jsx";
+
+function App() {
+
+
+  return (
+    <>
+        <Header />
+        <Home />
+    </>
+  )
+}
+
+export default App

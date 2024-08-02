9ffb52f75dfaa4f8e894bfcfa4caa82c808b1b02
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-08-03 00:18:24 +0400

    made stories grid😁

--- src/stories/stories.jsx
+++ src/stories/stories.jsx
@@ -1,6 +1,12 @@
 import "./stories.css";
-import MountainsImage from "../assets/stories/desktop/mountains.jpg";
+import stories from "../works.json";
+import {useEffect, useState} from "react";
 export default function Stories() {
+    const [storiesData, setStoriesData] = useState([]);
+
+    useEffect(() => {
+        setStoriesData(stories)
+    }, []);
     return (
         <main>
             <section className="big-image">
@@ -30,24 +36,26 @@ export default function Stories() {
                 </div>
             </section>
             <section id="works">
-                <a href="#">
-                    <div className="story-details">
-                        <h3 className="story-details__place">The Mountains</h3>
-                        <p className="story-details__name">by John Appleseed</p>
-                        <div className="line"></div>
-                        <div className="invite read-story">
-                            <button type="button" className="invite-button">Read the Story</button>
-                            <svg className="invite-button__arrow" xmlns="http://www.w3.org/2000/svg"
-                                 width="43"
-                                 height="14">
-                                <g fill="none" fillRule="evenodd" stroke="#000">
-                                    <path d="M0 7h41.864M35.428 1l6 6-6 6"/>
-                                </g>
-                            </svg>
+                {storiesData.map((story) => (
+                    <a href="#" key={story.id}>
+                        <div className="story-details">
+                            <h3 className="story-details__place">{story.title}</h3>
+                            <p className="story-details__name">{story.title}</p>
+                            <div className="line"></div>
+                            <div className="invite read-story">
+                                <button type="button" className="invite-button">Read the Story</button>
+                                <svg className="invite-button__arrow" xmlns="http://www.w3.org/2000/svg"
+                                     width="43"
+                                     height="14">
+                                    <g fill="none" fillRule="evenodd" stroke="#000">
+                                        <path d="M0 7h41.864M35.428 1l6 6-6 6"/>
+                                    </g>
+                                </svg>
+                            </div>
                         </div>
-                    </div>
-                    <img src={MountainsImage} alt="Mountains"/>
-                </a>
+                        <img src={story.image} alt="Mountains"/>
+                    </a>
+                ))}
             </section>
         </main>
     )

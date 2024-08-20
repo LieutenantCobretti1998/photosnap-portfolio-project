730cd77b8d2a4ae8026a434cad1fdbccfb8e0fd6
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-08-21 00:02:16 +0400

    work with packages table

--- src/pricing/pricing-plan.jsx
+++ src/pricing/pricing-plan.jsx
@@ -1,14 +1,14 @@
 
-function PricingPlan({title, description, price}) {
+function PricingPlan({title, description, price, status}) {
     return (
-        <div className="pricing-plan">
+        <div className={`pricing-plan ${status}`}>
             <div className="title-descr">
                 <h2>{title}</h2>
                 <p>{description}</p>
             </div>
             <div className="price-pr-mnth">
                 <h3>{price}</h3>
-                <p>per month</p>
+                <p>{status}</p>
             </div>
             <div className="pricing">
 
--- src/pricing/pricing.jsx
+++ src/pricing/pricing.jsx
@@ -1,12 +1,16 @@
 import WomanShooting from "../assets/pricing/desktop/hero.jpg";
 import "./pricing.css";
-import pricingPlan from "./pricing-plan.jsx";
 import pricing from "../pricing.json";
 import {useState} from "react";
 import PricingPlan from "./pricing-plan.jsx";
 
 export default function Pricing() {
     const [plans, setPlans] = useState(pricing);
+    console.log(plans)
+    const [yearly, setYearly] = useState(false);
+    const handleChange = () => {
+        setYearly(!yearly);
+    }
     return (
         <main>
             <section id="section-1">
@@ -27,18 +31,75 @@ export default function Pricing() {
 
             <section className="price-list">
                 <div className="price-changer">
-                    <h3 className="monthly">Monthly</h3>
+                    <h3 className={`monthly ${!yearly ? 'active' : ''}`}>Monthly</h3>
                     <label className="switch round" htmlFor="switch-mode">
-                        <input id="switch-mode" placeholder="" type="checkbox"/>
+                        <input onChange={handleChange} id="switch-mode" placeholder="" type="checkbox"/>
                         <span className="slider round"></span>
                     </label>
-                    <h3 className="yearly">Yearly</h3>
+                    <h3 className={`yearly ${yearly ? 'active' : ''}`}>Yearly</h3>
                 </div>
-                <div className="prices">
+                <div className={`prices ${yearly ? 'yearly-active' : 'monthly-active'}`}>
                     {plans.map((plan, index) => (
-                        <PricingPlan key={index} price={plan.price} title={plan.title} description={plan.description} />
+                        <PricingPlan key={index}
+                                     price={yearly ? plan.price_yearly : plan.price_monthly}
+                                     title={plan.title}
+                                     description={plan.description}
+                                     status={yearly ? "per year" : "per month" }
+                        />
                     ))}
                 </div>
+            </section>
+
+            <section className="packages-section">
+                <h2>Compare</h2>
+                <table className="pricing-table">
+                    <thead>
+                    <tr>
+                        <th>The Features</th>
+                        <th>Basic</th>
+                        <th>Pro</th>
+                        <th>Business</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Unlimited Story Posting</td>
+                        <td>✔</td>
+                        <td>✔</td>
+                        <td>✔</td>
+                    </tr>
+                    <tr>
+                        <td>Unlimited Photo Upload</td>
+                        <td>✔</td>
+                        <td>✔</td>
+                        <td>✔</td>
+                    </tr>
+                    <tr>
+                        <td>Embedding Custom Content</td>
+                        <td></td>
+                        <td>✔</td>
+                        <td>✔</td>
+                    </tr>
+                    <tr>
+                        <td>Customize Metadata</td>
+                        <td></td>
+                        <td>✔</td>
+                        <td>✔</td>
+                    </tr>
+                    <tr>
+                        <td>Advanced Metrics</td>
+                        <td></td>
+                        <td></td>
+                        <td>✔</td>
+                    </tr>
+                    <tr>
+                        <td>Photo Downloads</td>
+                        <td></td>
+                        <td></td>
+                        <td>✔</td>
+                    </tr>
+                    </tbody>
+                </table>
 
             </section>
         </main>

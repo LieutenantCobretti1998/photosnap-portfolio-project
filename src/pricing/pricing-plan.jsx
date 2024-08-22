71836d62e6356c183fe0a7c8e4588ff91f3c10d5
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-08-22 18:55:29 +0400

    added new footer component

--- src/pricing/pricing-plan.jsx
+++ src/pricing/pricing-plan.jsx
@@ -1,18 +1,71 @@
+import {AnimatePresence, motion} from 'framer-motion';
+const variants = {
+    hidden: {
+        opacity: 0,
+        y: 50,
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 0.5,
+            ease: 'easeOut',
+        }
+    }
+}
 
-function PricingPlan({title, description, price, status}) {
+const contentVariants = {
+    hiddenDown: {
+        opacity: 0,
+        y: -100,
+        transition: {duration: 0.5},
+    },
+    hiddenTop: {
+        opacity: 0,
+        y: 100,
+        transition: {duration: 0.5},
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {duration: 0.5},
+    }
+}
+const tableVariants = {
+    hidden: {opacity: 0, y: 20},
+    visible: {opacity: 1, y: 0, transition: {duration: 0.5},},
+}
+function PricingPlan({title, description, price, status, isYearly}) {
     return (
-        <div className={`pricing-plan ${status} ${title === 'Pro' ? 'pro' : ''}`}>
-            <div className="title-descr">
-                <h2>{title}</h2>
-                <p>{description}</p>
-            </div>
-            <div className="price-pr-mnth">
-                <h3>{price}</h3>
-                <p>{status}</p>
-            </div>
+        <motion.div
+            className={`pricing-plan ${status} ${title === 'Pro' ? 'pro' : ''}`}
+            initial="hidden"
+            variants={variants}
+            animate="visible"
+            transition={{duration: 0.2, ease: "easeIn"}}
+            whileHover={{scale: 1.01, boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.2)"}}
+        >
+            <AnimatePresence mode="wait">
+                <motion.div
+                    key={isYearly ? 'yearly' : 'monthly'}
+                    variants={contentVariants}
+                    initial={isYearly ? 'hiddenTop' : 'hiddenDown'}
+                    animate="visible"
+                    exit={isYearly ? 'hiddenDown' : 'hiddenTop'}
+                >
+                    <div className="title-descr">
+                        <h2>{title}</h2>
+                        <p>{description}</p>
+                    </div>
+                    <div className="price-pr-mnth">
+                        <h3>{price}</h3>
+                        <p>{status}</p>
+                    </div>
+                </motion.div>
+            </AnimatePresence>
 
-            <a href="#" type="button" className="choose-btn">PICK PLAN</a>
-        </div>
+            <a href="#" type="button" className={`choose-btn ${title === 'Pro' ? 'white-btn' : ''}`}>PICK PLAN</a>
+        </motion.div>
     );
 }
 

0a3ac2b6db401858b8671d27c086d695cef8f300
LieutenantCobretti1998 <djamil5099@gmail.com>
2024-08-28 23:44:15 +0400

    working on responsive design of stories page👍👌💵

--- src/stories/stories.jsx
+++ src/stories/stories.jsx
@@ -5,6 +5,9 @@ import {motion, useAnimation, useInView} from "framer-motion";
 import Footer from "../footer/footer.jsx";
 import {Link, Outlet, useParams} from "react-router-dom";
 import storyLoader from "../data_loading.js";
+import MountainsImageSmall from "../assets/stories/mobile/moon-of-appalacia.jpg";
+import MountainsImageMedium from "../assets/stories/tablet/moon-of-appalacia.jpg";
+import ResponsiveImage from "../helpers/responsiveImage.jsx";
 
 const imageVariants = {
     hidden: {
@@ -22,6 +25,17 @@ const imageVariants = {
             ease: "easeInOut",
         }
     })
+}
+
+const breakPoints = {
+    "mobile": {
+        "min": 330,
+        "max": 600
+    },
+    "tablet": {
+        "min": 600,
+        "max": 1280
+    },
 }
 
 export default function Stories() {
@@ -69,38 +83,80 @@ export default function Stories() {
 
     return (
         <main>
-
-            <motion.section
-                className="big-image"
-                initial={{y: -20, opacity: 0}}
-                animate={{y: 0, opacity: 1}}
-                transition={{duration: .5, ease: "easeIn"}}
-            >
-                <div className="overview">
-                    <p className="overview__featured">
-                        <b>Last month's featured story</b>
-                    </p>
-                    <h1 className="overview__name">
-                        Hazy full moon of Appalachia
-                    </h1>
-                    <p className="overview__time">March 2nd 2020 by John Appleseed</p>
-                    <span className="overview__story">
-                        The dissected plateau area, while not actually made up of geological mountains,
-                        is popularly called "mountains", especially in eastern Kentucky and West Virginia,
-                        and while the ridges are not high, the terrain is extremely rugged.
-                    </span>
-                    <div className="invite read-story-1">
-                        <button type="button" className="invite-button">Read the Story</button>
-                        <svg className="invite-button__arrow" xmlns="http://www.w3.org/2000/svg"
-                             width="43"
-                             height="14">
-                            <g fill="none" fillRule="evenodd" stroke="#000">
-                                <path d="M0 7h41.864M35.428 1l6 6-6 6"/>
-                            </g>
-                        </svg>
-                    </div>
-                </div>
-            </motion.section>
+            {
+                window.innerWidth > breakPoints.mobile.min && window.innerWidth < breakPoints.mobile.max ? (
+                    <motion.section
+                        className="big-image"
+                        initial={{y: -20, opacity: 0}}
+                        animate={{y: 0, opacity: 1}}
+                        transition={{duration: .5, ease: "easeIn"}}
+                    >
+                        <ResponsiveImage alt="Man looking on the lake"
+                                         smallSrc={MountainsImageSmall}
+                                         mediumSrc={MountainsImageMedium}
+                                         defaultSrc={MountainsImageMedium}
+                        />
+                        <div className="black-box">
+                            <div className="overview">
+                                <p className="overview__featured">
+                                    <b>Last month's featured story</b>
+                                </p>
+                                <h1 className="overview__name">
+                                    Hazy full moon of Appalachia
+                                </h1>
+                                <p className="overview__time">March 2nd 2020 by John Appleseed</p>
+                                <span className="overview__story">
+                                    The dissected plateau area, while not actually made up of geological mountains,
+                                    is popularly called "mountains", especially in eastern Kentucky and West Virginia,
+                                    and while the ridges are not high, the terrain is extremely rugged.
+                                </span>
+                                <div className="invite read-story-1">
+                                    <button type="button" className="invite-button">Read the Story</button>
+                                    <svg className="invite-button__arrow" xmlns="http://www.w3.org/2000/svg"
+                                         width="43"
+                                         height="14">
+                                        <g fill="none" fillRule="evenodd" stroke="#000">
+                                            <path d="M0 7h41.864M35.428 1l6 6-6 6"/>
+                                        </g>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                    </motion.section>
+                ) : (
+                    <motion.section
+                        className="big-image"
+                        initial={{y: -20, opacity: 0}}
+                        animate={{y: 0, opacity: 1}}
+                        transition={{duration: .5, ease: "easeIn"}}
+                    >
+                        <div className="overview">
+                            <p className="overview__featured">
+                                <b>Last month's featured story</b>
+                            </p>
+                            <h1 className="overview__name">
+                                Hazy full moon of Appalachia
+                            </h1>
+                            <p className="overview__time">March 2nd 2020 by John Appleseed</p>
+                            <span className="overview__story">
+                                The dissected plateau area, while not actually made up of geological mountains,
+                                is popularly called "mountains", especially in eastern Kentucky and West Virginia,
+                                and while the ridges are not high, the terrain is extremely rugged.
+                            </span>
+                            <div className="invite read-story-1">
+                                <button type="button" className="invite-button">Read the Story</button>
+                                <svg className="invite-button__arrow" xmlns="http://www.w3.org/2000/svg"
+                                     width="43"
+                                     height="14">
+                                    <g fill="none" fillRule="evenodd" stroke="#000">
+                                        <path d="M0 7h41.864M35.428 1l6 6-6 6"/>
+                                    </g>
+                                </svg>
+                            </div>
+                        </div>
+                    </motion.section>
+                )
+            }
             <motion.section id="works" ref={storiesContainer}>
                 {storiesData.map((story) => (
                     <motion.div
@@ -131,7 +187,14 @@ export default function Stories() {
                                 </svg>
                             </div>
                         </div>
-                        <motion.img src={story.image}
+                        <motion.img srcSet={`
+                                        ${story.imageSmall} 600w,
+                                        ${story.image}
+                                    `}
+                                    sizes="
+                                            (max-width: 600px) 100vw,
+                                            (max-width: 1200px) 100vw,
+                                        "
                                     className="stories-image"
                                     alt={story.alt}
                         />
